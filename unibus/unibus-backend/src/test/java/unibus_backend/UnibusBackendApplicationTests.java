983a4fdf31b648e43882adecea47171c49e8a3fd
isabelle victoria <isabellevic69@gmail.com>
2026-09-06 21:09:15 -0300

    chore: adiciona configuracao global de CORS para comunicacao com o React

---
+++ unibus/unibus-backend/src/test/java/unibus_backend/UnibusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package unibus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnibusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
