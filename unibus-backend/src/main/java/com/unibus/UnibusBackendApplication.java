58c8f8255fe5d42d1d393b7261c5d9556a26e85d
Isabelle Victória <isabellevic69@gmail.com>
2026-09-10 11:19:44 -0300

    fix: ajusta pacote base da aplicação backend

--- unibus-backend/src/main/java/com/unibus/UnibusBackendApplication.java
+++ unibus-backend/src/main/java/com/unibus/UnibusBackendApplication.java
@@ -1,4 +1,4 @@
-package unibus_backend;
+package com.unibus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UnibusBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnibusBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UnibusBackendApplication.class, args);
+    }
 }
