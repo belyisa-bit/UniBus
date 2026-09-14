1ee8afbe15848a5b73266e3219482fa79c4b7019
gabriellyrcarneiro <gabriellyrcarneiros@gmail.com>
2026-09-14 18:59:44 -0300

    KAN-33: criar componente Card

--- unibus-frontend/src/pages/Home.jsx
+++ unibus-frontend/src/pages/Home.jsx
@@ -1,5 +1,16 @@
+import Card from "../components/Card/Card";
+
 function Home() {
-  return <h1>Home - UniBus</h1>
+  return (
+    <div>
+      <h1>Home - UniBus</h1>
+
+      <Card>
+        <h2>Linha Centro</h2>
+        <p>Saída às 07:30</p>
+      </Card>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
