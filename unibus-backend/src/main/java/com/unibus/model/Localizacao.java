bc38b8a159ea5f95560dab8b158bcd299492567f
Isabelle Victoria <isabellevic69@gmail.com>
2026-09-18 11:14:30 -0300

    feat: adiciona validacoes de Localizacao

--- unibus-backend/src/main/java/com/unibus/model/Localizacao.java
+++ unibus-backend/src/main/java/com/unibus/model/Localizacao.java
@@ -15,10 +15,17 @@ public class Localizacao {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @DecimalMin("-90.0")
+    @DecimalMax("90.0")
     private BigDecimal latitude;
 
+    @NotNull
+    @DecimalMin("-180.0")
+    @DecimalMax("180.0")
     private BigDecimal longitude;
 
+    @NotNull
     private LocalDateTime dataHora;
 
     @ManyToOne
