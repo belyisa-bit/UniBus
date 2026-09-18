09b9c28a8cc32cd501a9c9a5a52ecd7e1d30f1d0
Isabelle Victoria <isabellevic69@gmail.com>
2026-09-18 10:28:21 -0300

    feat: cria OnibusRepository

---
+++ unibus-backend/src/main/java/com/unibus/repository/OnibusRepository.java
@@ -0,0 +1,7 @@
+package ...repository;
+
+import ...model.Onibus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OnibusRepository extends JpaRepository<Onibus, Long> {
+}
