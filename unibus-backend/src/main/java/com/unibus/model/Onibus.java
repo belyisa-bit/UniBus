e1300886ffbaadf7169bf9954ae6b26fa0317cf1
Isabelle Victoria <isabellevic69@gmail.com>
2026-09-22 11:27:25 -0300

    feat(backend): implementa simulador de coordenadas GPS agendado

--- unibus-backend/src/main/java/com/unibus/model/Onibus.java
+++ unibus-backend/src/main/java/com/unibus/model/Onibus.java
@@ -1,9 +1,21 @@
-package ...model;
+package com.unibus.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "onibus")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Onibus {
 
     @Id
@@ -19,5 +31,4 @@ public class Onibus {
     @ManyToOne
     @JoinColumn(name = "linha_id")
     private Linha linha;
-
 }
--- unibus-backend/src/main/java/com/unibus/repository/OnibusRepository.java
+++ unibus-backend/src/main/java/com/unibus/repository/OnibusRepository.java
@@ -1,8 +1,12 @@
-import ...model.Onibus;
-import ...service.OnibusService;
+package com.unibus.repository;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import com.unibus.model.Onibus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OnibusRepository extends JpaRepository<Onibus, Long> {
+    Optional<Onibus> findByIdentificacao(String identificacao);
+}
